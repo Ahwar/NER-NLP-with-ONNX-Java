5967d9e61f6064b504e97aad56e23ecf7de87ba5
ahwar <20405797+Ahwar@users.noreply.github.com>
2022-11-18 23:51:38 +0500

    feat: onnx inference with dummy input
    Loading and doing inference on the model using onnx-runtime with dummy input
    
    process:
        create onnx environment and session
        Create dummy input and convert to OnnxTensor
        Running the inference on the model
        Handling the output
    
    Signed-off-by: ahwar <20405797+Ahwar@users.noreply.github.com>

--- src/main/java/dev/ahwar/Main.java
+++ src/main/java/dev/ahwar/Main.java
@@ -1,28 +1,63 @@
 package dev.ahwar;
 
+import ai.onnxruntime.OnnxTensor;
 import ai.onnxruntime.OrtEnvironment;
 import ai.onnxruntime.OrtException;
 import ai.onnxruntime.OrtSession;
+import ai.onnxruntime.OrtSession.Result;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class Main {
 
 
-    public static void main(String[] args) {
-        try {
-            OrtEnvironment env = OrtEnvironment.getEnvironment();
-            String model_path = "C:\\Users\\pakle\\IdeaProjects\\onnx-rt\\src\\main\\java\\dev\\ahwar\\roberta-sequence-classification-9.onnx";
-            OrtSession session = env.createSession(model_path, new OrtSession.SessionOptions());
-            System.out.println(session.getInputInfo());
-            System.out.println(session.getInputNames());
+    public static void main(String[] args) throws IOException {
+        OnnxTensor inputTensor;
 
-            // TODO: how to do BertTokenizer? In Python:
-            // from transformers import BertTokenizerFast
-            // need this for Java...
-            // Inference with Java BERT NLP Deep Learning and ONNX Runtime
-            // session.run(null);
+        try {
+            /*
+             * Creating ONNX environment and session
+             * */
+            OrtEnvironment env = OrtEnvironment.getEnvironment();
+            String model_path = "src/main/java/dev/ahwar/roberta-base-11.onnx";
+            // create an onnx-runtime session
+            OrtSession session = env.createSession(model_path, new OrtSession.SessionOptions());
+
+
+            /*
+             * input and output layers Info
+             * */
+            System.out.printf("Model Input Names:  %s\nModel Input info:  %s\n" +
+                            "Model Output Names:  %s\nModel Output info:  %s",
+                            session.getInputNames(), session.getInputInfo(),
+                            session.getOutputNames(), session.getOutputInfo());
+
+            /*
+             * Create dummy input and convert to OnnxTensor
+             * */
+            // dummy inputs
+            long[][] inputArray = new long[1][20];
+            // create tensor
+            inputTensor = OnnxTensor.createTensor(env, inputArray);
+            // map inputTensor according to model Input
+            var model_inputs = Map.of("input_ids", inputTensor);
+
+
+            /*
+             * Running the inference on the model
+             * */
+            Result output = session.run(model_inputs);
+
+            /*
+            * Handling the output
+            * */
+            float[][][] output_1 = (float[][][]) output.get(0).getValue();
+            float[][] output_2 = (float[][]) output.get(1).getValue();
+
         } catch (OrtException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Hello world!");
     }
 }
